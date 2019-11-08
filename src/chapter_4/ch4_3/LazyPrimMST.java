8619fc8b54bd0217e69f2a7109e5f1df15e4ee89
Kei <huang.jch@outlook.com>
2019-11-08 15:49:31 +0800

    4.3

---
+++ src/chapter_4/ch4_3/LazyPrimMST.java
@@ -0,0 +1,48 @@
+package chapter_4.ch4_3;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+public class LazyPrimMST {
+    private boolean[] marked;
+    private Queue<Edge> mst;
+    private MinPQ<Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph G){
+        pq=new MinPQ<>();
+        marked=new boolean[G.V()];
+        mst=new Queue<>();
+
+        visit(G, 0);
+        while(!pq.isEmpty()){
+            Edge e=pq.delMin();
+
+            int v=e.either(), w=e.other(v);
+            if(marked[v]&&marked[w])
+                continue;
+            mst.enqueue(e);
+            if(!marked[v])
+                visit(G, v);
+            if(!marked[w])
+                visit(G, w);
+        }
+    }
+
+    private void visit(EdgeWeightedGraph G, int v){
+        marked[v]=true;
+        for(Edge e:G.adj(v))
+            if(!marked[e.other(v)])
+                pq.insert(e);
+    }
+
+    public Iterable<Edge> edges(){
+        return mst;
+    }
+
+    public double weight(){
+        double weight=0;
+        for(Edge e:mst)
+            weight+=e.weight();
+        return weight;
+    }
+}
